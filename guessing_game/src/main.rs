0f22b19d2e15122768092316100c77e244248ad3
William <william@mrjaeger.net>
2024-12-05 14:53:07 +0300

    Take in a guess and return the result

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,9 +1,27 @@
+use rand::prelude::*;
 use std::io;
 
 fn main() {
-    print!("Guess a number: ");
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    print!("Guess a number from 1 to 100: ");
 
-    println!("You guessed: {}", guess);
+    let screte_number: u8 = rand::thread_rng().gen_range(1..=100);
+
+    let mut guess = String::new();
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+    let guess = match guess.trim().parse::<u8>() {
+        Ok(num) => num,
+        Err(_) => panic!("Please type a number!"),
+    };
+
+    println!(
+        "You guessed \"{}\" is {}.",
+        guess,
+        match guess.eq(&screte_number) {
+            true => "right🎆",
+            false => "wrong⛔",
+        }
+    );
 }
