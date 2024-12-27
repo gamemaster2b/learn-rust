7e7247b00dafee832e97086a5824963ac29c2fe5
William <william@mrjaeger.net>
2024-12-27 05:55:21 +0300

    notes

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,17 +1,20 @@
-/// Constants are declared using the 'const' keyword are immutable.<br/>
-/// They can not be made mutable. Adding 'mut' will result in a compile time error.
+// tag::constants-a[]
 const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
+// end::constants-a[]
 
 fn main() {
+    // tag::constants-b[]
     println!("There are {THREE_HOURS_IN_SECONDS} seconds in 3 hours."); // THREE_HOURS_IN_SECONDS = 10800
+    // end::constants-b[]
 
-    // Variables are declared using the 'let' keyword and immutable by default.
     // tag::declaration[]
+    let z;
     let y = 18;
-    println!("The value of x is: {y}"); // y = 18
+    z = 93;
+    println!("The value of y is: {y}"); // y = 18
+    println!("The value of z is: {z}"); // z = 93
     // end::declaration[]
 
-    // 'mut' is used to make a variable mutable.
     // tag::mutability[]
     let mut x = 5;
     println!("The value of x is: {x}"); // x = 5
@@ -19,8 +22,6 @@ fn main() {
     println!("The value of x is: {x}"); //x = 6
     // end::mutability[]
 
-    // Shadowing is the declaration of new variable using the name of a preexisting variable
-    // The compiler identifies the second variable by the name until it leaves the scope or the second variable shadowed by another
     // tag::shadowing[]
     {
         let x = x * 2;
