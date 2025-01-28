39554c2913adee379269418ac541b11e449a5679
William <william@mrjaeger.net>
2025-01-28 22:21:43 +0300

    show the output

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -4,30 +4,30 @@ const THREE_HOURS_IN_SECONDS: u32 = 60 * 60 * 3;
 
 fn main() {
     // tag::constants-b[]
-    println!("There are {THREE_HOURS_IN_SECONDS} seconds in 3 hours."); // THREE_HOURS_IN_SECONDS = 10800
+    println!("There are {THREE_HOURS_IN_SECONDS} seconds in 3 hours."); // outPut: There are 10800 seconds in 3 hours.
     // end::constants-b[]
 
     // tag::declaration[]
     let z;
     let y = 18;
     z = 93;
-    println!("The value of y is: {y}"); // y = 18
-    println!("The value of z is: {z}"); // z = 93
+    println!("The value of y is: {y}"); // outPut: The value of y is: 18
+    println!("The value of z is: {z}"); // outPut: The value of y is: 93
     // end::declaration[]
 
     // tag::mutability[]
     let mut x = 5;
-    println!("The value of x is: {x}"); // x = 5
+    println!("The value of x is: {x}"); // outPut: The value of y is: 5
     x = 6;
-    println!("The value of x is: {x}"); //x = 6
+    println!("The value of x is: {x}"); // outPut: The value of y is: 6
     // end::mutability[]
 
     // tag::shadowing[]
     {
         let x = x * 2;
-        println!("The value of x in the inner scope is: {x}"); // x = 12
+        println!("The value of x in the inner scope is: {x}"); // outPut: The value of x in the inner scope is: 12
     }
 
-    println!("The value of x is: {x}"); // x = 6
+    println!("The value of x is: {x}"); // outPut: The value of y is: 6
     // end::shadowing[]
 }
