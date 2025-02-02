10b52e6728ffb632f7c4f91e52766df640af4fa1
William <william@mrjaeger.net>
2025-02-02 18:26:41 +0300

    start structs

--- structs-enums/src/main.rs
+++ structs-enums/src/main.rs
@@ -1,3 +1,14 @@
+#![allow(unused)]
+
+// tag::define-struct[]
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+// end::define-struct[]
+
 fn main() {
     println!("Hello, world!");
 }
