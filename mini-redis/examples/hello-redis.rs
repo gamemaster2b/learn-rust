1aba089d9be61a6ee8c4ce60818a3754305e481c
William <william@mrjaeger.net>
2025-03-06 19:00:37 +0300

    hello-redis

---
+++ mini-redis/examples/hello-redis.rs
@@ -0,0 +1,27 @@
+use mini_redis::{Result, client};
+
+async fn say() {
+    println!("World!");
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    let name = say();
+
+    print!("Hello, ");
+
+    name.await;
+
+    // Open a connection to mini-redis
+    let mut client = client::connect("127.0.0.1:6379").await?;
+
+    // Set the key "hello" with value "world"
+    client.set("hello", "world!".into()).await?;
+
+    //get key "hello"
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
+}
