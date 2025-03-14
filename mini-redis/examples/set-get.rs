40a4ca50d2e547e84572029391c4436de8f29d13
William <william@mrjaeger.net>
2025-03-14 14:03:39 +0300

    used to test cross-thread cashing

---
+++ mini-redis/examples/set-get.rs
@@ -0,0 +1,48 @@
+use mini_redis::client;
+use tokio::task::JoinHandle;
+
+#[tokio::main]
+async fn main() {
+    let task_names: [&str; 3] = ["Alice", "Bella", "Crystall"];
+    println!(
+        "There are three tasks to run async writing and reading to the redis instance: {}, {} and {}\n",
+        task_names[0], task_names[1], task_names[2]
+    );
+
+    let mut handles: Vec<JoinHandle<()>> = Vec::new();
+
+    for name in task_names {
+        let handle: JoinHandle<()> = tokio::spawn(async move {
+            task(name).await;
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.await.unwrap();
+    }
+}
+
+async fn task(name: &str) {
+    let mut client = client::connect("127.0.0.1:6379").await.unwrap();
+
+    for key in 1..=10 {
+        if let Some(value) = client.get(&(key.to_string())[..]).await.unwrap() {
+            println!("{} read: {:?}", name, value);
+        } else {
+            match client
+                .set(
+                    &(key.to_string())[..],
+                    format!("{} wrote {}", name, key).into(),
+                )
+                .await
+            {
+                Ok(_) => {
+                    println!("{} wrote {}", name, key)
+                }
+                Err(e) => {
+                    eprintln!("{} failed to write {} because {:?}", name, key, e)
+                }
+            };
+        }
+    }
+}
